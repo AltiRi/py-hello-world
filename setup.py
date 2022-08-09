bc0846f1049fa0969beab4596ab305063a7a058f
AltiRi <48589192+AltiRi@users.noreply.github.com>
2022-08-09 11:09:59 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from setuptools import setup
 setup(
     name="py-hello-world",
     version="0.0.1",
+    packages=[
+        "py_hello_world",
+    ],
     entry_points={
         "console_scripts": [
             "py-hello-world = py_hello_world.main:say_hi",
